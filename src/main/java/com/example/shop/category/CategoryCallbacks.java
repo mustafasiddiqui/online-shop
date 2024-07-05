5f277312a883815e078b659b321a8ae82cf578e4
Mustafa Siddiqui <20732444+mustafasiddiqui@users.noreply.github.com>
2024-07-04 20:12:35 -0500

    Only set id if not already set

--- src/main/java/com/example/shop/category/CategoryCallbacks.java
+++ src/main/java/com/example/shop/category/CategoryCallbacks.java
@@ -1,20 +1,18 @@
 package com.example.shop.category;
 
-import org.bson.Document;
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.core.mapping.event.AfterConvertCallback;
 import org.springframework.data.mongodb.core.mapping.event.BeforeConvertCallback;
-import org.springframework.data.mongodb.core.mapping.event.BeforeSaveCallback;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 public class CategoryCallbacks implements BeforeConvertCallback<Category> {
 
     @Override
     public Category onBeforeConvert(Category entity, String collection) {
-        entity.setId(collection + "-" + new ObjectId());
+        if (entity.getId() == null) {
+            entity.setId(collection + "-" + new ObjectId());
+        }
+
         return entity;
     }
 }
