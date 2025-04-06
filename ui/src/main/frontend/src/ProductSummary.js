870d86da74bceebe604078bc60b3b2745b5908fa
Mustafa Siddiqui <20732444+mustafasiddiqui@users.noreply.github.com>
2025-04-06 10:04:56 -0500

    Cap size of product cards and images

--- ui/src/main/frontend/src/ProductSummary.js
+++ ui/src/main/frontend/src/ProductSummary.js
@@ -3,8 +3,8 @@ import Card from 'react-bootstrap/Card';
 
 export default function ProductSummary({ product }) {
   return (
-    <Card border="primary">
-      {product.url != null && <Card.Img src={product.url} />}
+    <Card border="primary" className="productSummary">
+      {product.url != null && <Card.Img className="productImage" src={product.url} />}
       <Card.Title>{product.name}</Card.Title>
       <Card.Body>
         <Card.Text>{product.price == null ? 'No price found.' : '$' + product.price.toFixed(2)}</Card.Text>
